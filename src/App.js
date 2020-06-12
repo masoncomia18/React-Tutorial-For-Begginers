da12003c1da6a598ea0b9f15a94e519d892836a9
Mason Comia <mason.comia18@yahoo.com>
2020-06-12 09:26:14 -0700

    Added an array of objects in the state Used the map method to map over the array Should now be displaying 3 tweets

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React, { useState } from "react";
 import Tweet from "./Tweet";
 
 function App() {
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  };
+  const [users, setUsers] = useState([
+    { name: "Mason", message: "I am a web developer." },
+    { name: "John", message: "My name is John Do." },
+    { name: "Harry", message: "Harry Styles is my favorite artist!" },
+  ]);
 
   return (
     <div className="app">
-      <h1 className={isRed ? "red" : ""}>Change my Color!</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {users.map((user) => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
